46900b7b129db7018392ded7eb7662ed3affa4bc
Mayank Chopra <makchopra@codeaurora.org>
2013-04-23 12:03:06 +0530

    msm_fb: display: Do a pre-fill to writeback buffers.
    When switching mdp operating mode to blt, writeback buffers need
    pre-fill. Move the pre-fill code from isr routine to do_blt, where
    actual switch to blt mode happens.
    
    CRs-fixed: 471098
    Change-Id: I0f845397894c749ac518a1fa605f84cd5fc2ad15
    Signed-off-by: Mayank Chopra <makchopra@codeaurora.org>
    
    Conflicts:
    	drivers/video/msm/mdp4_overlay_dsi_video.c

--- drivers/video/msm/mdp4_overlay_dsi_video.c
+++ drivers/video/msm/mdp4_overlay_dsi_video.c
@@ -198,18 +198,6 @@ int mdp4_dsi_video_pipe_commit(int cndx, int wait)
 	}
 	spin_unlock_irqrestore(&vctrl->spin_lock, flags);
 
-	if (vctrl->blt_change) {
-		pipe = vctrl->base_pipe;
-		spin_lock_irqsave(&vctrl->spin_lock, flags);
-		INIT_COMPLETION(vctrl->dmap_comp);
-		INIT_COMPLETION(vctrl->ov_comp);
-		vsync_irq_enable(INTR_DMA_P_DONE, MDP_DMAP_TERM);
-		spin_unlock_irqrestore(&vctrl->spin_lock, flags);
-		mdp4_dsi_video_wait4dmap(0);
-		if (pipe->ov_blt_addr)
-			mdp4_dsi_video_wait4ov(0);
-	}
-
 	pipe = vp->plist;
 
 	for (i = 0; i < OVERLAY_PIPE_MAX; i++, pipe++) {
@@ -906,17 +894,6 @@ void mdp4_dmap_done_dsi_video(int cndx)
 		mdp_ov0_blt_ctl == MDP4_BLT_SWITCH_TG_ON_ISR) {
 		mdp4_overlayproc_cfg(pipe);
 		mdp4_overlay_dmap_xy(pipe);
-		if (pipe->ov_blt_addr) {
-			mdp4_dsi_video_blt_ov_update(pipe);
-			pipe->ov_cnt++;
-			
-			vsync_irq_enable(INTR_OVERLAY0_DONE,
-						MDP_OVERLAY0_TERM);
-			
-			mdp4_stat.kickoff_ov0++;
-			vctrl->ov_koff++;	
-			outpdw(MDP_BASE + 0x0004, 0);
-		}
 		vctrl->blt_change = 0;
 	}
 
@@ -1003,6 +980,17 @@ static void mdp4_dsi_video_do_blt(struct msm_fb_data_type *mfd, int enable)
 		}
 		mdp4_overlayproc_cfg(pipe);
 		mdp4_overlay_dmap_xy(pipe);
+		if (pipe->ov_blt_addr) {
+			mdp4_dsi_video_blt_ov_update(pipe);
+			pipe->ov_cnt++;
+			/* Prefill one frame */
+			vsync_irq_enable(INTR_OVERLAY0_DONE,
+						MDP_OVERLAY0_TERM);
+			/* kickoff overlay0 engine */
+			mdp4_stat.kickoff_ov0++;
+			vctrl->ov_koff++;	/* make up for prefill */
+			outpdw(MDP_BASE + 0x0004, 0);
+		}
 		if (tg_enabled) {
 			if (pipe->ov_blt_addr) {
 				spin_lock_irqsave(&vctrl->spin_lock, flag);
