491c1df8f384e0934210941f421ee95b4b11dd6a
Srinu Gorle <sgorle@codeaurora.org>
2013-05-13 20:05:04 +0530

    msm: vidc: Do not reset VCD_FRAME_FLAG_DATACORRUPT flag for VC1
    - To propagate VCD_FRAME_FLAG_DATACORRUPT flag to IL client, dont reset.
    
    Change-Id: I68bf4c953c02a2a4327fe6e875863853cb4e0bf8
    CRs-Fixed: 472798
    Signed-off-by: Srinu Gorle <sgorle@codeaurora.org>
    
    Conflicts:
    	drivers/video/msm/vidc/1080p/ddl/vcd_ddl_interrupt_handler.c

--- drivers/video/msm/vidc/1080p/ddl/vcd_ddl_interrupt_handler.c
+++ drivers/video/msm/vidc/1080p/ddl/vcd_ddl_interrupt_handler.c
@@ -1258,6 +1258,19 @@ static u32 ddl_decoder_output_done_callback(
 				output_vcd_frm->flags |=
 					VCD_FRAME_FLAG_DATACORRUPT;
 		}
+		if (decoder->codec.codec != VCD_CODEC_H264 &&
+			decoder->codec.codec != VCD_CODEC_MPEG2 &&
+			decoder->codec.codec != VCD_CODEC_VC1)
+			output_vcd_frm->flags &= ~VCD_FRAME_FLAG_DATACORRUPT;
+		if (decoder->codec.codec == VCD_CODEC_MPEG2) {
+			vidc_sm_get_mp2common_status(&ddl->shared_mem
+				[ddl->command_channel],
+				&seq_end_code_present);
+			if (seq_end_code_present)
+				output_vcd_frm->flags |= VCD_FRAME_FLAG_EOSEQ;
+			else
+				output_vcd_frm->flags &= ~VCD_FRAME_FLAG_EOSEQ;
+		}
 		output_vcd_frm->ip_frm_tag = dec_disp_info->tag_top;
 		vidc_sm_get_picture_times(&ddl->shared_mem
 			[ddl->command_channel],
