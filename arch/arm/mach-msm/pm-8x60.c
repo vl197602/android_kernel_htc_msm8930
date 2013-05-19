22581c21ea838b55d0f19b26898f1f5fd6d8c93d
Venkat Devarasetty <vdevaras@codeaurora.org>
2013-05-19 16:36:48 +0530

    msm: pm: add failed stats
    for idle and suspend failed cases the stats
    are not updated correctly, they are just added to
    success bucket.
    
    This patch is to log the failed stats separately.
    
    Change-Id: I4b290944e3f4b74ada13ef0ba51b812c83b5ec5d
    Signed-off-by: Venkat Devarasetty <vdevaras@codeaurora.org>
    Signed-off-by: Sridhar Gujje <sgujje@codeaurora.org>
    
    Conflicts:
    	arch/arm/mach-msm/pm-8x60.c

--- arch/arm/mach-msm/pm-8x60.c
+++ arch/arm/mach-msm/pm-8x60.c
@@ -1006,8 +1006,11 @@ int msm_pm_idle_enter(enum msm_pm_sleep_mode sleep_mode)
 		break;
 
 	case MSM_PM_SLEEP_MODE_POWER_COLLAPSE_STANDALONE:
-		msm_pm_power_collapse_standalone(true);
-		exit_stat = MSM_PM_STAT_IDLE_STANDALONE_POWER_COLLAPSE;
+		if(msm_pm_power_collapse_standalone(true))
+			exit_stat = MSM_PM_STAT_IDLE_STANDALONE_POWER_COLLAPSE;
+		else
+			exit_stat =
+			     MSM_PM_STAT_IDLE_FAILED_STANDALONE_POWER_COLLAPSE;
 		break;
 
 	case MSM_PM_SLEEP_MODE_POWER_COLLAPSE: {
@@ -1017,7 +1020,7 @@ int msm_pm_idle_enter(enum msm_pm_sleep_mode sleep_mode)
 		int ret = -ENODEV;
 		int notify_rpm =
 			(sleep_mode == MSM_PM_SLEEP_MODE_POWER_COLLAPSE);
-		int collapsed;
+		int collapsed = 0;
 
 		timer_expiration = msm_pm_timer_enter_idle();
 
@@ -1052,7 +1055,10 @@ int msm_pm_idle_enter(enum msm_pm_sleep_mode sleep_mode)
 				msm_pm_add_stat(MSM_PM_STAT_IDLE_POWER_COLLAPSE_VDD_MIN, vdd_min_time);
 		}
 		msm_pm_timer_exit_idle(timer_halted);
-		exit_stat = MSM_PM_STAT_IDLE_POWER_COLLAPSE;
+		if (collapsed)
+			exit_stat = MSM_PM_STAT_IDLE_POWER_COLLAPSE;
+		else
+			exit_stat = MSM_PM_STAT_IDLE_FAILED_POWER_COLLAPSE;
 		break;
 	}
 
@@ -1253,6 +1259,7 @@ static int msm_pm_enter(suspend_state_t state)
 		struct msm_rpmrs_limits *rs_limits = NULL;
 		int ret = -ENODEV;
 		uint32_t power;
+		int collapsed = 0;
 
 		if (MSM_PM_DEBUG_SUSPEND & msm_pm_debug_mask)
 			pr_info("%s: power collapse\n", __func__);
@@ -1289,9 +1296,6 @@ static int msm_pm_enter(suspend_state_t state)
 						msm_pm_max_sleep_time,
 						rs_limits, false, true);
 			if (!ret) {
-				xo_shutdown_time = msm_rpm_get_xo_time();
-				vdd_min_time = msm_rpm_get_vdd_time();
-
 				collapsed = msm_pm_power_collapse(false);
 				if (pm_sleep_ops.exit_sleep) {
 					pm_sleep_ops.exit_sleep(rs_limits,
@@ -1315,7 +1319,10 @@ static int msm_pm_enter(suspend_state_t state)
 		keep_dig_voltage_low_in_idle(true);
 
 		time = msm_pm_timer_exit_suspend(time, period);
-		msm_pm_add_stat(MSM_PM_STAT_SUSPEND, time);
+		if (collapsed)
+			msm_pm_add_stat(MSM_PM_STAT_SUSPEND, time);
+		else
+			msm_pm_add_stat(MSM_PM_STAT_FAILED_SUSPEND, time);
 	} else if (allow[MSM_PM_SLEEP_MODE_POWER_COLLAPSE_STANDALONE]) {
 		if (MSM_PM_DEBUG_SUSPEND & msm_pm_debug_mask)
 			pr_info("%s: standalone power collapse\n", __func__);
