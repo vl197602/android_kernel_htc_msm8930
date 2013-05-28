d634f5ee2888db7a60916e4dac2b6d56face5208
Carter Cooper <ccooper@codeaurora.org>
2013-05-28 17:07:13 -0600

    msm: kgsl: Add device init function
    Some device specific parameters need to be setup only once during
    device initialization. Create an init function for this purpose
    rather than re-doing this init everytime the device is started.
    
    Change-Id: I45c7fcda8d61fd2b212044c9167b64f793eedcda
    Signed-off-by: Carter Cooper <ccooper@codeaurora.org>
    
    Conflicts:
    	drivers/gpu/msm/adreno.c

--- drivers/gpu/msm/adreno_ringbuffer.c
+++ drivers/gpu/msm/adreno_ringbuffer.c
@@ -304,7 +304,7 @@ int adreno_ringbuffer_load_pfp_ucode(struct kgsl_device *device)
 	return 0;
 }
 
-int adreno_ringbuffer_start(struct adreno_ringbuffer *rb, unsigned int init_ram)
+int adreno_ringbuffer_start(struct adreno_ringbuffer *rb)
 {
 	int status;
 	
@@ -315,9 +315,6 @@ int adreno_ringbuffer_start(struct adreno_ringbuffer *rb, unsigned int init_ram)
 
 	if (rb->flags & KGSL_FLAGS_STARTED)
 		return 0;
-
-	if (init_ram)
-		rb->global_ts = 0;
 
 	kgsl_sharedmem_set(&rb->memptrs_desc, 0, 0,
 			   sizeof(struct kgsl_rbmemptrs));
