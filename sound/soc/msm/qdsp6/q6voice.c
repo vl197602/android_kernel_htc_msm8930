4ea5313ae5f04bd581a3a3f933992ea2c88b296a
Venkat Sudhir <vsudhir@codeaurora.org>
2013-06-17 14:21:14 -0700

    ASoC: msm: qdsp6v2: Fix bug in device V2 command
    Add missing braces for Device V2 command failure case.
    Crs-fixed: 492994
    
    Change-Id: Ieacbbe94897a4fb73e6ffadc7d1bdcadb2b35dd2
    Signed-off-by: Venkat Sudhir <vsudhir@codeaurora.org>
    Signed-off-by: Sridhar Gujje <sgujje@codeaurora.org>
    
    Conflicts:
    	sound/soc/msm/qdsp6/q6voice.c

--- sound/soc/msm/qdsp6/q6voice.c
+++ sound/soc/msm/qdsp6/q6voice.c
@@ -3176,8 +3176,9 @@ int voc_enable_cvp(uint16_t session_id)
 		if (common.ec_ref_ext == true) {
 			ret = voice_send_set_device_cmd_v2(v);
 			if (ret < 0) {
-				pr_err("%s:  set device V2 failed " \
-					"rc =%x\n", __func__, ret);
+				pr_err("%s: set device V2 failed\n"
+				       "rc =%x\n", __func__, ret);
+				goto fail;
 			}
 		} else {
 			ret = voice_send_set_device_cmd(v);
