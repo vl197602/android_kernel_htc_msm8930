e15f6c2d6d7fa7d56159e4e2e882f5a6629e03b1
Shubhraprakash Das <sadas@codeaurora.org>
2013-06-20 13:53:24 -0600

    msm: kgsl: Prevent race conditions when freeing memory
    Multiple threads can call an ioctl to free a memory region.
    Only one of these threads must be allowed to free memory and
    the rest can return without freeing. Add a new pending flag
    which is tested and set in a critical section guarded by
    spinlock to prevent multiple ioctl threads from freeing the
    same memory.
    Also, a thread could be freeing a memory region that is
    in use by another thread. Ensure that the detachment of the
    memory from the process list and freeing of the memory always
    happens in the same thread. This will prevent a situation where
    the memory is being used by a thread which is detached from the
    process list and is yet to be freed.
    Seperate the selection of gpuaddr and mapping into pagetable. The
    gpuaddr assignment needs to be done with process memory lock held
    in a critical section but the mapping into pagetable can be done
    separately without holding memory lock.
    
    CRs-Fixed: 495144
    Change-Id: Idf85fbd4bca29c18597f4b0e737c207f002ab266
    Signed-off-by: Shubhraprakash Das <sadas@codeaurora.org>
    
    Conflicts:
    	drivers/gpu/msm/kgsl.c

--- drivers/gpu/msm/kgsl.h
+++ drivers/gpu/msm/kgsl.h
@@ -144,6 +144,8 @@ struct kgsl_memdesc_ops {
 #define KGSL_MEMDESC_GLOBAL BIT(1)
 /* The memdesc is frozen during a snapshot */
 #define KGSL_MEMDESC_FROZEN BIT(2)
+/* The memdesc is mapped into a pagetable */
+#define KGSL_MEMDESC_MAPPED BIT(3)
 
 struct kgsl_memdesc {
 	struct kgsl_pagetable *pagetable;
@@ -194,6 +196,8 @@ struct kgsl_mem_entry {
 	unsigned int id;
 	unsigned int context_id;
 	struct kgsl_process_private *priv;
+	/* Initialized to 0, set to 1 when entry is marked for freeing */
+	int pending_free;
 };
 
 #ifdef CONFIG_MSM_KGSL_MMU_PAGE_FAULT
